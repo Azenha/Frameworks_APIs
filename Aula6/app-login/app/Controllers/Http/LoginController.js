baa505f27d2b02dc9488f27417e9964f075c347a
Leonardo Azenha <Azenha@users.noreply.github.com>
2021-09-08 22:07:44 -0300

    Aula 6

---
+++ Aula6/app-login/app/Controllers/Http/LoginController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class LoginController {
+    async index({view, session}){
+        let username = session.get('username', '');
+        return view.render('index', {user: username});
+    }
+
+    async login({response, session}){
+        let username = session.get('username', '');
+        if(username){
+            session.clear();
+        }
+        else{
+            session.put('username', 'Logado')
+        }
+        response.redirect('back');
+    }
+}
+
+module.exports = LoginController
