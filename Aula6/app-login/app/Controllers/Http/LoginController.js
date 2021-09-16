a06dc6edbe4b7cba2c0a4a9063d0c9ca6d6379c4
Azenha <azenha.leonardo@gmail.com>
2021-09-15 21:59:02 -0300

    aula 7

--- Aula6/app-login/app/Controllers/Http/LoginController.js
+++ Aula6/app-login/app/Controllers/Http/LoginController.js
@@ -1,20 +1,41 @@
 'use strict'
 
+const Usuario = use("App/Models/Usuario");
+
 class LoginController {
-    async index({view, session}){
-        let username = session.get('username', '');
-        return view.render('index', {user: username});
+    async index({ view }) {
+        return view.render('login');
     }
 
-    async login({response, session}){
-        let username = session.get('username', '');
-        if(username){
-            session.clear();
+    async login({ request, response, session }) {
+        const usuario = new Usuario();
+        usuario.email = request.input('email');
+        usuario.senha = request.input('senha');
+        usuario.nome = "Administrador";
+
+        if (usuario.email == "admin@admin.com" && usuario.senha == "12345") {
+            console.log("Logado com sucesso!");
+            session.put('nome', usuario.nome);
+            return response.redirect('/bemvindo')
         }
-        else{
-            session.put('username', 'Logado')
+        else {
+            console.log("Usuário e/ou senha inválidos!");
+            session.flash({
+                notificacao: "Usuário e/ou senha inválidos!"
+            })
+            return response.redirect('back');
         }
-        response.redirect('back');
+    }
+
+    async bemVindo({ view, session, response }) {
+        const nome = session.get('nome');
+        if(nome) {
+            return view.render('bemvindo', {nome: nome});
+        }
+        else {
+            response.redirect('/')
+        }
+        
     }
 }
 
